2d0b7e00869f83d73aba34e9c6f25e2dc01751b0
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-06-04 16:03:59 +0530

    getusers and adduser api

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,39 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/api/v1/") 
 public class UserController {
 
-    @GetMapping("/getUser")
-    public String getUser() {
-        //   return new String();
-        return "All users";
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers() {
+       
+        return  userService.getAllUsers();
     }
+
+    @PostMapping("/adduser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+
+        
+        return userService.saveUser(userDTO);
+    }
+    
 
 }
--- src/main/java/com/example/demo/repo/UserRepo.java
+++ src/main/java/com/example/demo/repo/UserRepo.java
@@ -1,5 +1,12 @@
 package com.example.demo.repo;
 
-public interface UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.example.demo.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    
 }
