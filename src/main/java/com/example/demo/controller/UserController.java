d57d9f07d1d17424a3e522e0743655fb1c342a53
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-06-07 16:35:19 +0530

    update delete

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.UserDTO;
 import com.example.demo.service.UserService;
+
 
 
 @RestController
@@ -33,6 +36,17 @@ public class UserController {
 
         
         return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser( @RequestBody UserDTO userDTO) {
+     
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteuser")
+    public String deleteUser( @RequestBody UserDTO userDTO) {
+        return userService.deleteUser(userDTO);
     }
     
 
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -34,4 +34,14 @@ public class UserService {
         return userDTO;
     }
 
+    public UserDTO updateUser(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO,User.class));
+        return userDTO;
+    }
+
+    public String deleteUser(UserDTO userDTO) {
+        userRepo.delete(modelMapper.map(userDTO, User.class));
+        return "User deleted successfully";
+    }
+
 }
